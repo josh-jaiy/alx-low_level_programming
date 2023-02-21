b6cbd976f8e07f274f4eb28bc1e9f1aeea0acd12
josh-jaiy <yisanet@gmail.com>
2023-02-21 14:34:00 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.c"
+
+/**
+ * main - Empty point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+        _putchar("c");
+       	_putchar("t");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n);
+
+	return(0);
+}
