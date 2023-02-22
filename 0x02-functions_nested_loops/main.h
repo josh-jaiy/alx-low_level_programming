3a13f93997be98e2c579d4e33e8ee949a9a155dd
josh-jaiy <yisanet@gmail.com>
2023-02-21 20:46:03 -0800

    my push

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,6 @@
-#ifndef  HOLBERTON_H_THCLUDED
-#define OLBERTON_H_INCLUDED
-
-int _ putchar(char);
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -9,11 +8,10 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_baver(void);
+void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void orint_times_table(int n);
-void num(int r, int n);
+void print_times_table(int n);
 
 #endif
