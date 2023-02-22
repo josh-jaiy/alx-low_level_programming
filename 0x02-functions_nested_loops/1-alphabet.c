3a0a34b4f104e9dae3982c3d4fd55f998e37ca9b
josh-jaiy <yisanet@gmail.com>
2023-02-21 21:01:47 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Alphabets
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
