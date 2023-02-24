a7e915dc0084bdb945fdef90d8235a4018cbfd36
josh-jaiy <yisanet@gmail.com>
2023-02-23 17:58:27 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - Print 0 to 9
+ * Return: empty
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		if (n == 4 || n == 2)
+			continue;
+		else
+		{
+			_putchar(n + '0');
+		}
+	}
+	_putchar('\n');
+}
+
