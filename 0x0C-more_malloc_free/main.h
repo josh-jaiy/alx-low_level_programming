6696dbbc4b236b19344825fc429f14bc8c5fe4d4
josh-jaiy <yisanet@gmail.com>
2023-03-16 15:41:27 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
-char **strtow(char *str);
+
+void _puts(char *str);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char c);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
+
+
 #endif
