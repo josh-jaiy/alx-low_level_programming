6cc1275bb1080f53604e9c0170b518eb87d479fb
josh-jaiy <yisanet@gmail.com>
2023-06-12 06:22:53 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
-* _puts_recursion -The function checks if the current character pointed to by y
-* print it using the putchar
+* _puts_recursion - a function prints a string, followed by a new line
+* @y: the printed is a  string
 * Return: returns nothing
 */
-
-
-void _puts_recursion(char *y) {
-    if (*y == '\0') {
-        putchar('\n');
-        return;
-    }
-
-    putchar(*y);
-    _puts_recursion(y + 1);
+void _puts_recursion(char *y)
+{
+	if (*y == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar (*y);
+	_puts_recursion((y + 1));
 }
-
